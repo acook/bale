134dc4978ee42d3fdd1f17a8dc97abb8a826005b
Anthony Cook <anthony.cook@bizbuilt.com>
2011-03-13 04:57:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bale::Application.config.secret_token = '334e0fa137044df5cd92d16a7ad43ea6fc33ecae1930f60cf98e769ff7dbef3440a6f737062476549e889c42253b96a76a796810faeff04693ecb4fc24f5dbab'
