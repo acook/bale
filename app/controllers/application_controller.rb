767ef1a6579776e16cae489463ebfcf747cf33a7
Anthony Cook <anthony.cook@bizbuilt.com>
2011-03-13 08:08:48 -0700

    Display the actor name so I can keep track.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_actor_name
+
+  def current_actor_name 
+    ENV['BALE_ACTOR_NAME'] || "Default"
+  end
 end
